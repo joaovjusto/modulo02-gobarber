4d767a3d5e939d24ec6330974ba6ffc69a96461a
João Justo <joaovjusto03>
2019-07-23 19:41:10 -0300

    Working JWToken

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
+import authConfig from '../../config/auth'
+
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
@@ -11,14 +13,12 @@ export default async (req, res, next) => {
   const [, token] = authHeader.split(' ');
 
   try {
-    const decoded = await promisify(jwt.verify)()(token, authConfig.secret);
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    console.log(decoded);
+    req.userId = decoded.id;
 
     return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token invalid' })
   }
-
-  return next();
 }
