8c6aa3e346b28152a1ef5eae403dc412c0227a23
João Justo <joaovjusto03>
2019-08-06 22:48:36 -0300

    Validações agendamento

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -7,18 +7,20 @@ import authConfig from '../../config/auth';
 class SessionController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      email: Yup.string().email().required(),
-      password: Yup.string().required()
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
     });
-  
-    if (!(await schema.isValid(req.body))){
+
+    if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
 
-    if(!user) {
+    if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
 
@@ -26,9 +28,9 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user; 
+    const { id, name } = user;
 
-    return res.json( {
+    return res.json({
       user: {
         id,
         name,
@@ -37,7 +39,7 @@ class SessionController {
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
-    })
+    });
   }
 }
 
